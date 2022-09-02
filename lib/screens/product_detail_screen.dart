de319d933d93aded2d5d0971e6daa7636857c3d0
PaulTA91 <parmst202@caledonian.ac.uk>
2022-09-02 12:27:39 +0100

    Lesson 197: Listening in different places and ways

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -10,10 +13,14 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productID = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findByID(productID);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
