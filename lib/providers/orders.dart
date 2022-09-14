d3d43ebe618d56a3eef4448b89a72914f9450d22
PaulTA91 <parmst202@caledonian.ac.uk>
2022-09-14 19:18:09 +0100

    Lesson 257: Storing orders on the web

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import 'cart.dart';
 
@@ -23,14 +26,36 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final url = Uri.parse(
+        'https://flutter-shop-app-87bde-default-rtdb.europe-west1.firebasedatabase.app/orders.json');
+    final timeStamp = DateTime.now();
+
+    final response = await http.post(
+      url,
+      body: jsonEncode(
+        {
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'qauntity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
+        },
+      ),
+    );
+
     _orders.insert(
       0,
       OrderItem(
-        id: DateTime.now().toString(),
+        id: jsonDecode(response.body)['name'],
         amount: total,
         products: cartProducts,
-        dateTime: DateTime.now(),
+        dateTime: timeStamp,
       ),
     );
     notifyListeners();
