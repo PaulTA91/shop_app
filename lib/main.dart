d7964048f269910c4b01093336feacd57c88a74c
PaulTA91 <parmst202@caledonian.ac.uk>
2022-09-16 11:46:31 +0100

    Lesson 265: Adding the Authetication Screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/cart_screen.dart';
+import '../screens/auth_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_products_screen.dart';
 
@@ -38,13 +39,14 @@ class MyApp extends StatelessWidget {
           ),
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
           CartScreen.routeName: (context) => CartScreen(),
           OrdersScreen.routeName: (context) => OrdersScreen(),
           UserProductsScreen.routeName: (context) => UserProductsScreen(),
           EditProductScreen.routeName: (context) => EditProductScreen(),
+          AuthScreen.routeName: (context) => AuthScreen(),
         },
       ),
     );
