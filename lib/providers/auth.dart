20c6b0e3dd39ba0a83b0f0c795061b1daddc0177
PaulTA91 <parmst202@caledonian.ac.uk>
2022-09-16 12:56:52 +0100

    Lesson 269: Handling Authentication Errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -13,17 +15,26 @@ class Auth with ChangeNotifier {
     final url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBM0t-c1zFlVwZ22NH2MHY_YyKKrwhFRJk');
 
-    final response = await http.post(
-      url,
-      body: jsonEncode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(jsonDecode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: jsonEncode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = jsonDecode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(
+          responseData['error']['message'],
+        );
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signUp(String email, String password) async {
