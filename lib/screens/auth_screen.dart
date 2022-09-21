85c0ab45f107c9fe3f366bee8eccdb62df74d3fe
PaulTA91 <parmst202@caledonian.ac.uk>
2022-09-21 13:04:56 +0100

    Lesson 283: AnimatedBuilder widget

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -132,11 +132,11 @@ class _AuthCardState extends State<AuthCard>
         curve: Curves.fastOutSlowIn,
       ),
     );
-    _heightAnimation.addListener(
-      () => setState(
-        () {},
-      ),
-    );
+    // _heightAnimation.addListener(
+    //   () => setState(
+    //     () {},
+    //   ),
+    // );
   }
 
   @override
@@ -237,14 +237,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(
-          minHeight: _heightAnimation.value.height,
-        ),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (context, child) => Container(
+            // height: _authMode == AuthMode.Signup ? 320 : 260,
+            height: _heightAnimation.value.height,
+            constraints: BoxConstraints(
+              minHeight: _heightAnimation.value.height,
+            ),
+            width: deviceSize.width * 0.75,
+            padding: EdgeInsets.all(16.0),
+            child: child),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
