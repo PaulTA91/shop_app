936ec2dbdbddce32415d4d99fbd48f933d9a3891
PaulTA91 <parmst202@caledonian.ac.uk>
2022-09-21 14:19:16 +0100

    Lesson 289: Animating Order boxes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -19,33 +19,39 @@ class _OrderItemState extends State<OrderItem> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          ListTile(
-            title: Text('£${widget.order.amount.toStringAsPrecision(4)}'),
-            subtitle: Text(
-              DateFormat('dd-MMM-yyyy hh:mm').format(widget.order.dateTime),
-            ),
-            trailing: IconButton(
-              icon: Icon(
-                _expanded ? Icons.expand_less : Icons.expand_more,
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height:
+          _expanded ? min(widget.order.products.length * 20.00 + 110, 200) : 95,
+      child: Card(
+        margin: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            ListTile(
+              title: Text('£${widget.order.amount.toStringAsPrecision(4)}'),
+              subtitle: Text(
+                DateFormat('dd-MMM-yyyy hh:mm').format(widget.order.dateTime),
               ),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
+              trailing: IconButton(
+                icon: Icon(
+                  _expanded ? Icons.expand_less : Icons.expand_more,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+              ),
             ),
-          ),
-          if (_expanded)
-            Container(
+            AnimatedContainer(
+              duration: Duration(milliseconds: 300),
               padding: EdgeInsets.symmetric(
                 horizontal: 15,
                 vertical: 4,
               ),
-              height: min(widget.order.products.length * 20.00 + 10, 100),
+              height: _expanded
+                  ? min(widget.order.products.length * 20.00 + 10, 100)
+                  : 0,
               child: ListView(
                 children: widget.order.products
                     .map((prod) => Row(
@@ -67,7 +73,8 @@ class _OrderItemState extends State<OrderItem> {
                     .toList(),
               ),
             )
-        ],
+          ],
+        ),
       ),
     );
   }
